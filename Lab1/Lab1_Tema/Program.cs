9d9fed0e29ce6eb9842565fa00ac13c6c55ff48f
Sîrbu Iulia-Georgiana <46359762+iuliagsirbu@users.noreply.github.com>
2023-10-11 23:16:34 +0300

    Laborator 1

---
+++ Lab1/Lab1_Tema/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Cerința: Două clase 'Angajat', 'Proiect'
+         Atribute 'Angajat': nume, prenume, salariu lista de proiecte la care lucreaza
+         Atribute 'Proiect': nume, durata
+         Se cere lista de Angajati, fiecare cu mai multe proiecte la care lucreaza si sa se afiseze 
+         informatii despre angajati si proiectele lor*/
+
+using Lab1_Tema;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Angajat angajat1 = new Angajat("Popescu", "Ion", 6000);
+        angajat1.Proiecte.Add(new Proiect("Aplicatie A", 6));
+        angajat1.Proiecte.Add(new Proiect("Aplicatie B", 3));
+
+        Angajat angajat2 = new Angajat("Ionescu", "Ana", 8700);
+        angajat2.Proiecte.Add(new Proiect("Aplicatie C", 8));
+
+        Angajat angajat3 = new Angajat();
+        angajat3.Nume = "John";
+        angajat3.Prenume = "Smith";
+        angajat3.Salariu = 4000;
+        angajat3.Proiecte.Add(new Proiect("Aplicatie D", 2));
+
+        List<Angajat> listaAngajati = new List<Angajat>
+        {
+            angajat1,
+            angajat2,
+            angajat3
+        };
+
+        foreach(var angajat in listaAngajati)
+        {
+            angajat.PrintProiecte();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Lab1/Lab1_Tema/Proiect.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Tema
+{
+    public class Proiect
+    {
+        public string Nume {  get; set; }
+        public int Durata { get; set; }
+
+        public Proiect() { }
+        public Proiect(string nume, int durata) 
+        {
+            Nume = nume;
+            Durata = durata;
+        }
+    }
+}
