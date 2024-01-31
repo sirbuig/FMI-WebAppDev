712abbe4d361c5791dcf2b3bd5b493fb913606f9
iuliagsirbu <sirbuiuliageorgiana@gmail.com>
2024-01-31 21:13:47 +0200

    Lab 7 - no readme

--- Laboratories/Laboratory 7/Lab7_24/lab7/src/app/demo/demo.component.ts
+++ Laboratories/Laboratory 7/Lab7_24/lab7/src/app/demo/demo.component.ts
@@ -1,19 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {MatCardModule} from "@angular/material/card";
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
 import {MatListModule} from '@angular/material/list';
+import {FormsModule} from "@angular/forms";
+import {MatInput} from "@angular/material/input";
 
 @Component({
   selector: 'app-demo',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatIconModule, MatDividerModule, MatButtonModule, MatListModule],
+  imports: [CommonModule, MatCardModule, MatIconModule, MatDividerModule, MatButtonModule, MatListModule, FormsModule, MatInput],
   templateUrl: './demo.component.html',
   styleUrl: './demo.component.scss'
 })
-export class DemoComponent {
+export class DemoComponent implements OnInit, OnDestroy, OnChanges {
+  constructor() {
+    let div = document.getElementById('titleContainer');
+    console.log('Constructor html element value', div);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+      throw new Error('Method not implemented.');
+  }
+  ngOnDestroy(): void {
+      throw new Error('Method not implemented.');
+  }
+  ngOnInit(): void {
+    let div = document.getElementById('titleContainer');
+    console.log('ngOnInit html element value', div);
+  }
+
   public title: string = 'Demo component';
   public counter: number = 0;
   public users = [
